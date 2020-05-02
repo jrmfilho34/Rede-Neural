a39342646e70b47c4456eb740a98964c9ba6d8ef
MILIANO FERNANDES DE OLIVEIRA JUNIOR <35247714+jrmfilho34@users.noreply.github.com>
2020-05-02 19:33:33 -0300

    Primeiro commit: importacao de bibliotecas

---
+++ Robo_07.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 19:19:22 2020
+
+@author: Miliano Fernandes de Oliveira Junior
+
+Descricao: Rede Neural Recorrente para previsao do WIN da bovespa. Multivariada.
+
+"""
+
+# importacao das bibliotecas
+import tensorflow as tf
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import pandas as pd
